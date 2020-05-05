e7b2b67051310900bd6f2d306ad428f4c573d96f
Ahmad Fz <ahmad.f.zadeh@icloud.com>
2020-05-05 19:28:18 +0430

    add new puzzle images

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,13 +4,20 @@ self.addEventListener('install', function (e) {
             return cache.addAll([
                 '/',
                 '/index.html',
-                '/js/bundle.js',
-                '/js/puzzle-worker.js',
                 '/css/app.css',
                 '/assets/sprites/tileset.png',
                 '/assets/sprites/tileset.json',
                 '/assets/sounds/click1.ogg',
-                '/assets/images/view-01.jpg',
+                '/assets/images/puzzle-01.jpg',
+                '/assets/images/puzzle-02.jpg',
+                '/assets/images/puzzle-03.jpg',
+                '/assets/images/puzzle-04.jpg',
+                '/assets/images/puzzle-05.jpg',
+                '/assets/images/puzzle-06.jpg',
+                '/assets/images/puzzle-07.jpg',
+                '/assets/images/puzzle-08.jpg',
+                '/assets/images/puzzle-09.jpg',
+                '/assets/images/puzzle-10.jpg',
             ]);
         })
     );
