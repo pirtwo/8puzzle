4db6f18fb89aaf96094d08beee711981c9b965ed
Ahmad Fz <ahmad.f.zadeh@icloud.com>
2020-05-09 14:01:01 +0430

    add speed and radius args

--- src/scenes/spinner.js
+++ src/scenes/spinner.js
@@ -4,24 +4,27 @@ import {
 } from 'pixi.js';
 
 export default class Spinner extends Scene {
-    constructor() {
+    constructor({
+        radius = 25,
+        spinnerRadius = 10,
+        speed = 20
+    }) {
         super();
 
         let ctx = new Graphics();
-        // ctx.lineStyle(2, 0x000000);
         ctx.beginFill(0x000000);
-        this.parentCircle = ctx.drawCircle(0, 0, 15);
+        this.parentCircle = ctx.drawCircle(0, 0, radius);
         ctx.position.set(0, 0);
         this.addChild(ctx);
 
         ctx = new Graphics();
         ctx.beginFill(0xffffff);
-        this.childCircle = ctx.drawCircle(0, 0, 5);
-        this.childCircle.pivot.set(7, 0);
+        this.childCircle = ctx.drawCircle(0, 0, spinnerRadius);
+        this.childCircle.pivot.set(spinnerRadius + 3, 0);
         ctx.position.set(0, 0);
         this.addChild(ctx);
 
-        this.childCircle.rotation = 20;
+        this.childCircle.rotation = speed;
     }
 
     update(delta) {
