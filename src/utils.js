26242d38c63c606533f395fae026ec5455df84cc
Ahmad Fz <ahmad.f.zadeh@icloud.com>
2020-05-10 09:55:08 +0430

    add some functions

--- src/utils.js
+++ src/utils.js
@@ -18,7 +18,7 @@ export function coordinateToIndex({
 export function swapChars(str, charA, charB) {
     let indexA = str.indexOf(charA),
         indexB = str.indexOf(charB);
-        
+
     if (indexA > indexB)[indexA, indexB] = [indexB, indexA];
 
     return str.substring(0, indexA) +
@@ -26,4 +26,44 @@ export function swapChars(str, charA, charB) {
         str.substring(indexA + 1, indexB) +
         str[indexA] +
         str.substring(indexB + 1)
+}
+
+/**
+ * return true if tileA is a top neighbor of tileB.
+ * 
+ * @param {object} tileA 
+ * @param {object} tileB 
+ */
+export function isTopNeighbor(tileA, tileB) {
+    return tileA.col == tileB.col && tileA.row + 1 == tileB.row;
+}
+
+/**
+ * return true if tileA is a bottom neighbor of tileB.
+ * 
+ * @param {object} tileA 
+ * @param {object} tileB 
+ */
+export function isBottomNeighbor(tileA, tileB) {
+    return tileA.col == tileB.col && tileA.row - 1 == tileB.row;
+}
+
+/**
+ * return true if tileA is a left neighbor of tileB.
+ * 
+ * @param {object} tileA 
+ * @param {object} tileB 
+ */
+export function isLeftNeighbor(tileA, tileB) {
+    return tileA.row == tileB.row && tileA.col + 1 == tileB.col;
+}
+
+/**
+ * return true if tileA is a right neighbor of tileB.
+ * 
+ * @param {object} tileA 
+ * @param {object} tileB 
+ */
+export function isRightNeighbor(tileA, tileB) {
+    return tileA.row == tileB.row && tileA.col - 1 == tileB.col;
 }
